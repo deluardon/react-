240760257125e907bc3b9ece2fc5dc0067d5b278
deluardon <160032360+deluardon@users.noreply.github.com>
2024-06-17 01:38:33 +0600

    madil commit

--- src/Details/Details.jsx
+++ src/Details/Details.jsx
@@ -3,20 +3,22 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
 const Details = () => {
-    const jobs = useLoaderData();
-    const { id } = useParams();
-    const idparseint = parseInt(id);
-    const job = jobs.find((job) => job.id === idparseint);
-    console.log(job);
-  
- 
+  const jobs = useLoaderData();
+  const  {id}  = useParams();
+  const idparseint = parseInt(id);
+  const job = jobs.find((job) => job.id === idparseint);
+  console.log(jobs);
+  console.log(job);
+
   return (
     <div className="bg-white py-36 text-black">
-        <h1 className="text-center text-5xl text-black font-medium">Job Details </h1>
+      <h1 className="text-center text-5xl text-black font-medium">
+        Job Details{" "}
+      </h1>
       <div>
-      <h1>Details{id}</h1>
+        <h1>Details{id}</h1>
+        <h1>{job.logo}</h1>
       </div>
-
     </div>
   );
 };
